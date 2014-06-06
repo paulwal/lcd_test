ff4b10ee9f113606ea16bbc71b325224e0405e60
Paul Walton <walton.paul@gmail.com>
2014-06-06 12:36:37 -0500

    Moved clear screen to end to avoid flashing

--- HelloUniverse.c
+++ HelloUniverse.c
@@ -13,11 +13,12 @@
 int main (void)
 {
     lcd_initialize();
-    lcd_clear_area(0, 0, 240, 8);
+    _delay_ms(1000);
     lcd_draw_string(5, 0, 0, "The lazy brown fox something something...");
     lcd_draw_string(5, 2, 1, "The New York Times");
     lcd_draw_string(5, 4, 2, "012345");
     _delay_ms(15000);
+    lcd_clear_area(0, 0, 240, 8);
     lcd_power_down();
 
     return 0;
